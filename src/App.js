ba25aab3bfa52cc1d716d29922235a7ee1060fa6
JorgeF010 <figueroajorge010@gmail.com>
2021-01-10 11:18:12 -0500

    this commit includes a lot of changes to the ui, app, searchbar, playlist, tracklist, track, and searchrelsutls components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,89 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Playlist from "./components/Playlist";
+import SearchBar from "./components/SearchBar";
+import SearchResults from "./components/SearchResults";
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
 
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        { name: "name1", artist: "artist1", album: "album1", id: 1 },
+        { name: "name2", artist: "artist2", album: "album2", id: 2 },
+        { name: "name3", artist: "artist3", album: "album3", id: 3 }
+      ],
+      playlistName: "Workout Playlist",
+      playlistTracks: [
+        {
+          name: "playlistName1",
+          artist: "playlistArtist1",
+          album: "playlistAlbum1",
+          id: 4
+        },
+        {
+          name: "playlistName2",
+          artist: "playlistArtist2",
+          album: "playlistAlbum2",
+          id: 5
+        },
+        {
+          name: "playlistName3",
+          artist: "playlistArtist3",
+          album: "playlistAlbum3",
+          id: 6
+        }
+      ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if(tracks.find(savedTrack => savedTrack.id === track.id)) { 
+      return;
+    }
+    tracks.push(track);
+    this.setState(
+      {
+        playlistTracks: tracks
+      }
+    );
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    const indexOfTrack = tracks.findIndex(savedTrack => savedTrack.id === track.id);
+    tracks.splice(indexOfTrack, 1);
+    this.setState(
+      {
+        playlistTracks: tracks
+      }
+    );
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span class="highlight">mmm</span>ing
+        </h1>
+        <div class="App">
+          <SearchBar />
+          <div class="App-playlist"></div>
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+          <Playlist
+            playlistName={this.state.playlistName}
+            playlistTracks={this.state.playlistTracks}
+            onRemove={this.removeTrack}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Track.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../styles/Track.css";
+
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action" onClick={this.removeTrack}>-</button>;
+    } else {
+      return <button className="Track-action" onClick={this.addTrack}>+</button>;
+    }
+  }
+
+  addTrack() {
+    this.props.onAdd(this.props.track);
+  }
+
+  removeTrack() {
+    this.props.onRemove(this.props.track);
+  }
+
+  render() {
+    const trackName = this.props.track.name;
+    const artistName = this.props.track.artist;
+    const albumName = this.props.track.album;
+
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{trackName}</h3>
+          <p>{`${artistName}  ${albumName}`}</p>
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  }
+}
+
+export default Track;
