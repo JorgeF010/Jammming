d6cb720daf1213f7ca7da1124a88b3f22470afc2
JorgeF010 <figueroajorge010@gmail.com>
2021-01-11 07:12:40 -0500

    Working on the Spotify API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,31 @@ import React from "react";
 import "../styles/SearchBar.css";
 
 class SearchBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    }
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  handleTermChange(e) {
+    this.setState({
+      term: e.target.value
+    });
+  }
+
+
   render() {
     return (
       <div class="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist"/>
         <button class="SearchButton">SEARCH</button>
       </div>
     );
